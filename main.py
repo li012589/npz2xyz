830f24e91f134a0d0dbe44e7e29409a57ea98e4a
Li SH <li012589li@outlook.com>
2019-06-19 21:22:30 +0800

    update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ parser.add_argument("-xyz",default=None,help = "name of the xyz file")
 parser.add_argument("-name",default=None,help = 'name of the data saved in npz file')
 parser.add_argument("-smile",default=None,help = "Smile expression of the molecule")
 parser.add_argument("-batch",default=100,type=int,help = "batch size of configurations to convert")
+parser.add_argument("-scaling",default=10,type=float,help = "scaling factor of npz data, default is for nm to ångströms")
 args = parser.parse_args()
 
 smiles = []
@@ -18,10 +19,10 @@ smiles = ''.join(smiles)
 N = len(smiles)
 
 with np.load(args.npz) as data:
-    argOne = data[args.name].reshape(-1,N,3)
+    argOne = data[args.name].reshape(-1,N,3)*args.scaling
 
 for n in range(args.batch):
     with open(args.xyz.replace(".xyz",str(n)+".xyz"),"w") as f:
-        f.write(str(N)+"\n")
+        f.write(str(N)+"\n"+args.smile+"\n")
         for i,cha in enumerate(smiles):
             f.write(cha+"  "+str(argOne[n,i,0])+"  "+str(argOne[n,i,1])+"  "+str(argOne[n,i,2])+"\n")
