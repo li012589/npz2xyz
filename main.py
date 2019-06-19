50e8dcd8b5a87166d37d4e2d2e0ed8b481a01578
Li SH <li012589li@outlook.com>
2019-06-19 22:10:18 +0800

    add more options

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ parser.add_argument("-name",default=None,help = 'name of the data saved in npz f
 parser.add_argument("-smile",default=None,help = "Smile expression of the molecule")
 parser.add_argument("-batch",default=100,type=int,help = "batch size of configurations to convert")
 parser.add_argument("-scaling",default=10,type=float,help = "scaling factor of npz data, default is for nm to ångströms")
+parser.add_argument("-join",action='store_true',help="store all into one xyz file")
 args = parser.parse_args()
 
 smiles = []
@@ -21,8 +22,17 @@ N = len(smiles)
 with np.load(args.npz) as data:
     argOne = data[args.name].reshape(-1,N,3)*args.scaling
 
-for n in range(args.batch):
-    with open(args.xyz.replace(".xyz",str(n)+".xyz"),"w") as f:
-        f.write(str(N)+"\n"+args.smile+"\n")
-        for i,cha in enumerate(smiles):
-            f.write(cha+"  "+str(argOne[n,i,0])+"  "+str(argOne[n,i,1])+"  "+str(argOne[n,i,2])+"\n")
+if args.join:
+    with open(args.xyz,"w") as f:
+        for n in range(args.batch):
+            f.write(str(N)+"\n"+args.smile+"\n")
+            for i,cha in enumerate(smiles):
+                f.write(cha+"  "+str(argOne[n,i,0])+"  "+str(argOne[n,i,1])+"  "+str(argOne[n,i,2])+"\n")
+
+
+else:
+    for n in range(args.batch):
+        with open(args.xyz.replace(".xyz",str(n)+".xyz"),"w") as f:
+            f.write(str(N)+"\n"+args.smile+"\n")
+            for i,cha in enumerate(smiles):
+                f.write(cha+"  "+str(argOne[n,i,0])+"  "+str(argOne[n,i,1])+"  "+str(argOne[n,i,2])+"\n")
