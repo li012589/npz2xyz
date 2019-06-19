85382a4c0c3b9b93a9e3fc4e16e9dd483d4610a2
Li SH <li012589li@outlook.com>
2019-06-19 20:01:52 +0800

    udapte

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-npz",default=None,help = "name of the npz file")
+parser.add_argument("-xyz",default=None,help = "name of the xyz file")
+parser.add_argument("-name",default=None,help = 'name of the data saved in npz file')
+parser.add_argument("-smile",default=None,help = "Smile expression of the molecule")
+parser.add_argument("-batch",default=100,type=int,help = "batch size of configurations to convert")
+args = parser.parse_args()
+
+smiles = []
+for cha in args.smile:
+    if cha.isalpha():
+        smiles.append(cha)
+smiles = ''.join(smiles)
+N = len(smiles)
+
+with np.load(args.npz) as data:
+    argOne = data[args.name].reshape(-1,N,3)
+
+for n in range(args.batch):
+    with open(args.xyz.replace(".xyz",str(n)+".xyz"),"w") as f:
+        f.write(str(N)+"\n")
+        for i,cha in enumerate(smiles):
+            f.write(cha+"  "+str(argOne[n,i,0])+"  "+str(argOne[n,i,1])+"  "+str(argOne[n,i,2])+"\n")
